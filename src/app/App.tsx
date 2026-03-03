bc44dd6cd20e6c5dce5c724e6c1698033e0917f1
J0wen <61209323+NEWO-J@users.noreply.github.com>
2026-03-03 14:39:32 -0600

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -124,7 +124,7 @@ function Hero() {
 
   return (
     <section className="w-full bg-[rgba(0,0,0,0.2)] border border-black relative overflow-hidden">
-      <div className="relative px-16 pt-16 pb-12 min-h-[815px]">
+      <div className="relative px-16 pt-16 pb-12 min-h-[850px]">
         {/* Background illustration */}
         <div className="absolute right-60 top-[-65px] w-[720px] h-[850px]">
           <AnchorScene modelUrl="/anchor.glb" />
